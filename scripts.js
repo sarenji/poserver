2fd172b0b956191e0bfb8d5edef05cec42729bda
Antar1011 <summer.kenster@gmail.com>
2012-04-09 11:33:33 -0300

    well that was just dumb.

--- scripts.js
+++ scripts.js
@@ -807,7 +807,7 @@ addOwnerCommand("playerCount", function(){
 });
 addOwnerCommand("reload", function() {
   var id          = this.id;
-  if (id != 199470 || id != 132199 || id != 120307) {//only aldaron, antar and sarenji may use this script
+  if (id != 199470 && id != 132199 && id != 120307) {//only aldaron, antar and sarenji may use this script
     announce(id, "You are not authorized to use this command.");
     return;
   }
